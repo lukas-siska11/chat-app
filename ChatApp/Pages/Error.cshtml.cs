c05d01a49390b9a68f11c46cf59c8b69ed0d61ae
Lukáš Šiška <lukas.siska@b2a.cz>
2022-03-26 00:27:41 +0100

    Cleanup layout

--- ChatApp/Pages/Error.cshtml.cs
+++ ChatApp/Pages/Error.cshtml.cs
@@ -5,19 +5,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace ChatApp.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-[IgnoreAntiforgeryToken]
-public class ErrorModel : PageModel
+public class ErrorPageModel : PageModel
 {
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-    private readonly ILogger<ErrorModel> _logger;
-
-    public ErrorModel(ILogger<ErrorModel> logger)
-    {
-        _logger = logger;
-    }
 
     public void OnGet()
     {
--- ChatApp/Pages/Index.cshtml.cs
+++ ChatApp/Pages/Index.cshtml.cs
@@ -1,17 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ChatApp.Pages;
 
-public class IndexModel : PageModel
+public class IndexPageModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
     public void OnGet()
     {
     }
